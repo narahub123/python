0b5f2074f19c41c3fb4b4c61283cf51335e8e6a4
narahub123 <parknara0213@gmail.com>
2025-11-10 17:09:39 +0900

    Feat: 사용자 입력과 출력

---
+++ chapter4/4-2.py
@@ -0,0 +1,21 @@
+# a = input()
+
+# print("유저가 입력한 것" + a) # 유저가 입력한 것안녕하세요
+
+
+# a = input("숫자를 입력하세요: " )
+
+# print(a) # 3 
+
+# print(type(a)) # str
+
+print('life' "is" "too short") # lifeistoo short
+print("lift" + "is" + "too short") # lifeistoo short
+
+print('life',"is","too short") # life is too short
+
+# 한 줄에 출력하기 
+for i in range(10):
+    # print(i, end = " ") # 0 1 2 3 4 5 6 7 8 9
+    print(i, end = " to the ") 
+    #0 to the 1 to the 2 to the 3 to the 4 to the 5 to the 6 to the 7 to the 8 to the 9 to the
